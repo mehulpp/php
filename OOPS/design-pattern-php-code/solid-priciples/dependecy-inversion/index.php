9f07b7cbea89978d7f6c7c8bed3228697c9de709
Mehul Prajapati <mehul22.mp@gmail.com>
2022-09-02 12:50:17 +0530

    Update index.php

--- OOPS/design-pattern-php-code/solid-priciples/dependecy-inversion/index.php
+++ OOPS/design-pattern-php-code/solid-priciples/dependecy-inversion/index.php
@@ -1,4 +1,8 @@
 <?php
+/**
+* High-level modules should not depend on low-level modules. Both should depend on abstractions.
+* Abstractions should not depend on details. Details should depend on abstractions.
+*/
 //abstraction
 interface ConnectionInterface
 {
